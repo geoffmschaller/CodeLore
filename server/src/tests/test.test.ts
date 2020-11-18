5ce9726525a7b4dcfbd6aca22515692cb94019c2
geoffmschaller <32310187+geoffmschaller@users.noreply.github.com>
2020-11-17 23:02:11 -0800

    Config changes and Jest Config

---
+++ server/src/tests/test.test.ts
@@ -0,0 +1,8 @@
+describe('Just a dummy test', () => {
+	it('should return true', () => {
+		expect(200).toEqual(200);
+	});
+	it('should return false', () => {
+		expect(500).not.toEqual(200);
+	})
+})
